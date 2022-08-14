db3031d12186cc948329c426955dfe20c2d9dc8b
Rodrigo Munoz <rm117579@gmail.com>
2022-08-14 17:16:09 -0500

    fix: edit readme

--- src/extension.ts
+++ src/extension.ts
@@ -15,7 +15,7 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("html-ts.gotoTs", () => {
+  let disposable = vscode.commands.registerCommand("relevant-files.goto", () => {
     // The code you place here will be executed every time your command is executed
     // if(vscode.window.activeTextEditor && sufficientEditorsOpen())
     // {

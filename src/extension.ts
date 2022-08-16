295e119158620509f321f08a5c6b72a0bfbb11fd
Rodrigo Munoz <rm117579@gmail.com>
2022-08-15 19:30:20 -0500

    fix: switching between html and ts files works for when the file is already open.

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import { goTo, sufficientEditorsOpen } from "./libs/functions";
+import { createEditor, goTo, HtmlTs, isOpen, moveLeft, moveRight, openLeft, openRight, sufficientEditorsOpen } from "./libs/functions";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -15,36 +15,46 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("relevant-files.goto", () => {
+  let disposable = vscode.commands.registerCommand("relevant-files.htmlts", () => {
     let htmlFile: string;
-    vscode.window.showInformationMessage("editor number: " + vscode.window.activeTextEditor?.document.valueOf());
     if(sufficientEditorsOpen()){
-      switch(vscode.window.activeTextEditor?.viewColumn?.toString())
+      switch(vscode.window.activeTextEditor?.viewColumn?.valueOf())
       {
-        case '1':
-          htmlFile = vscode.window.activeTextEditor?.document.fileName;
-          // vscode.window.showInformationMessage("one editor open");
-          vscode.commands.executeCommand("workbench.action.focusSecondEditorGroup");
-          goTo(htmlFile);
+        // if the active editor is the first editor
+        case 1:
+          vscode.window.showInformationMessage("active editor is the first editor");
+          htmlFile = HtmlTs(vscode.window.activeTextEditor?.document.fileName);
+          if(isOpen(htmlFile))
+            {moveRight(htmlFile);}
+          else
+            {openRight(htmlFile);}
           break;
-          case '2':
-          htmlFile = vscode.window.activeTextEditor?.document.fileName;
-          // vscode.window.showInformationMessage("two editors open");
-          vscode.commands.executeCommand("workbench.action.focusFirstEditorGroup");
-          goTo(htmlFile);
+        // if the active editor is the second editor
+        case 2:
+          vscode.window.showInformationMessage("active editor is the second editor");
+          htmlFile = HtmlTs(vscode.window.activeTextEditor?.document.fileName);
+          if(isOpen(htmlFile))
+            {moveLeft(htmlFile);}
+          else
+            {openLeft(htmlFile);}
           break;
         default:
+          vscode.window.showInformationMessage("active editor is neither the first nor the second editor");
           if(vscode.window.activeTextEditor)
-            {goTo(vscode.window.activeTextEditor.document.fileName);}
+          {
+            htmlFile = HtmlTs(vscode.window.activeTextEditor?.document.fileName);
+            goTo(vscode.window.activeTextEditor.document.fileName);
+          }
           break;
       }
-    }else if(vscode.window.activeTextEditor){
-      // vscode.window.showInformationMessage("not enough editors, opening another editor");
-      vscode.commands.executeCommand("workbench.action.splitEditorRight");
-      vscode.commands.executeCommand("workbench.action.focusSecondEditorGroup");
-      goTo(vscode.window.activeTextEditor.document.fileName);
     }
-//     // workbench.action.splitEditor
+    // if there is only one editor open, split the window
+    // and open the file in the newly created second editor
+    else if(vscode.window.activeTextEditor){
+      vscode.window.showInformationMessage("not enough editors, opening another editor");
+      htmlFile = HtmlTs(vscode.window.activeTextEditor?.document.fileName);
+      createEditor(htmlFile)
+    }
   });
 
   context.subscriptions.push(disposable);
--- src/libs/functions.ts
+++ src/libs/functions.ts
@@ -1,17 +1,42 @@
 
 import * as vscode from "vscode";
 
+
+/**
+ * Most useful for angular projects.
+ * This function will alternate between relevant html and ts files.
+ * 
+ * @param file : the file name to convert
+ * @returns 
+ */
+export function HtmlTs(file: string): string
+{
+  let newFile: string = '';
+  if(file.endsWith(".html"))
+    {newFile = file.replace(".html", ".ts");}
+  else if(file.endsWith(".ts"))
+    {newFile = file.replace(".ts", ".html");}
+  
+  return newFile;
+}
+
+/**
+ * 
+ * @param file : the file to open
+ */
 export function goTo(file: string ) {
-  //convert the html file to ts file
-  let newFile: string = file.replace(".html", ".ts");
-  //open the newFile
-  vscode.workspace.openTextDocument(newFile).then(doc => {
+  vscode.workspace.openTextDocument(file).then(doc => {
     vscode.window.showTextDocument(doc);
   });
 }
 
+/**
+ * 
+ * @returns true if there are two or more editors open
+ */
 export function sufficientEditorsOpen(){
   let sufficientEditors: boolean = false;
+  vscode.window.showInformationMessage("checking for sufficient editors: " + vscode.window.visibleTextEditors.length);
   if(vscode.window.visibleTextEditors.length >= 2){
     sufficientEditors = true;
   }
@@ -20,10 +45,45 @@ export function sufficientEditorsOpen(){
 
 
 
-export function openInFirstEditor(){
-
+/**
+ * Opens a file in the first editor.
+ * @param file : the file to open
+ */
+export function openLeft(file: string){
+  vscode.commands.executeCommand("workbench.action.focusFirstEditorGroup");
+  goTo(file);
 }
 
-export function openInSecondEditor(){
+/**
+ * Opens a file in the second editor.
+ * @param file : the file to open
+ */
+export function openRight(file: string){
+  vscode.commands.executeCommand("workbench.action.focusSecondEditorGroup");
+  goTo(file);
+}
 
+export function createEditor(file: string){
+  vscode.commands.executeCommand("workbench.action.splitEditorRight");
+  vscode.commands.executeCommand("workbench.action.focusSecondEditorGroup");
+  goTo(file);
+}
+
+export function isOpen(fileToOpen: string){
+  let open: boolean = false;
+  vscode.workspace.textDocuments.forEach(openFile => {
+    if(openFile.fileName === fileToOpen){
+      open = true;
+    }
+  });
+  return open;
+}
+
+export function moveLeft(file: string){
+  goTo(file);
+  vscode.commands.executeCommand("workbench.action.moveEditorToPreviousGroup");
+}
+export function moveRight(file: string){
+  goTo(file);
+  vscode.commands.executeCommand("workbench.action.moveEditorToNextGroup");
 }

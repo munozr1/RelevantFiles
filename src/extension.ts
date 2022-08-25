5f3fcc23dafecf8830d2ebea8aa6de70591b4011
Rodrigo Munoz <rm117579@gmail.com>
2022-08-25 11:59:35 -0500

    mis

--- src/extension.ts
+++ src/extension.ts
@@ -11,11 +11,6 @@ import { createConfigFile, linkFiles, returnConfig } from "./libs/config";
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-
-
-
-
-
   let disposable = vscode.commands.registerCommand("relevant-files.htmlts", () => {
     let htmlFile: string;
     if(sufficientEditorsOpen()){
